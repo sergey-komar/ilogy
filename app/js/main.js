e934576a5bf2879d1f049a1339664cf4390f10e5
sergey-komar <kursev81@yandex.ru>
2022-10-21 23:51:05 +0300

    start

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function () {
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        appendArrows: '.slider__arrows',
+        prevArrow: '<button  type="button" class="slick-next"></button>',
+        nextArrow: '<button type="button" class="slick-prev"></button>'
+    });
 
+    const menuLink = document.querySelectorAll('.menu-bottom__list-item');
+    const menuOverflow = document.querySelector('.menu-bottom__overflow');
+
+    menuLink.forEach(item=>{
+        item.addEventListener('click', ()=>{
+            menuOverflow.classList.toggle('hide');
+        })
+    })
 })
